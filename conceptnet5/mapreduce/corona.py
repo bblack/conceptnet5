3ddd8ac6002d1f902c29ebf106770270d7eb66b3
Rob Speer <rspeer@mit.edu>
2011-12-16 15:33:00 -0500

    this is the secondary sort thing that doesn't work

--- conceptnet5/mapreduce/corona.py
+++ conceptnet5/mapreduce/corona.py
@@ -9,62 +9,109 @@ def parallel(a, b):
     return float(a*b) / (a + b)
 
 class CORONAForwardStep(MRJob):
+    PARTITIONER = 'org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner'
+
+    def __init__(self, *args, **kwargs):
+        self.current_node = None
+        self.current_value = None
+        MRJob.__init__(self, *args, **kwargs)
+
     def map_json(self, key, value):
         # first step: the actual key is None
         value = simplejson.loads(value)
         startURI = value['start']
         endURI = value['end']
-        weight = value['weight']
+        weight = value.get('weight', 1.0)
         type = value['type']
-        if not ('\t' in startURI or '\t' in endURI):
-            edge_value = u"edge\t%(type)s\t%(end)s\t%(weight)s" % dict(
+        if not ('|' in startURI or '|' in endURI):
+            edge_value = u"%(type)s|%(end)s|%(weight)s" % dict(
                 type=type, end=endURI, weight=weight
             )
-            yield startURI, edge_value
+            yield startURI+"|edge", edge_value
             score = 0
             if key == '/':
                 score = 1.0
-            node_value = u"NODE\t%s" % (score*weight)
-            yield endURI, node_value
+            node_value = str(score*weight)
+            yield startURI+"|NODE", '0'
+            yield endURI+"|NODE", node_value
 
     def reduce_activation(self, key, values):
+        key, objtype = key.split('|')
+
+        conjunction = key.startswith('/conjunction')
+        if objtype == 'NODE':
+            if key == '/':
+                sum = 1.
+            else:
+                sum = None
+                for value in values:
+                    val2 = float(value)
+                    if sum is None:
+                        sum = val2
+                    else:
+                        if conjunction:
+                            sum = parallel(sum, val2)
+                        else:
+                            sum += val2
+            self.current_node = key
+            self.current_value = sum
+
+        elif objtype == 'edge':
+            if key == '/':
+                self.current_node = '/'
+                self.current_value = 1.
+            for value in values:
+                parts = value.split('|')
+                if len(parts) < 3:
+                    raise ValueError(str(values))
+                if self.current_node != key:
+                    # Set a flag value for when this happens (because it
+                    # shouldn't)
+                    self.current_node = key
+                    self.current_value = 0.0123
+                type = parts[0]
+                end = parts[1]
+                weight = float(parts[2])
+                node_score = self.current_value * weight
+                edge_value = u"%(type)s|%(end)s|%(weight)s" % dict(
+                  type=type, end=end, weight=weight
+                )
+                yield key+"|edge", edge_value
+                node_value = str(node_score)
+                yield end+"|NODE", node_value
+        else:
+            raise ValueError(objtype)
+    
+    def reduce_nop(self, key, values):
+        yield key, list(values)
+
+    def reduce_final(self, key, values):
+        key, objtype = key.split('|')
         conjunction = key.startswith('/conjunction')
         sum = None
-        if key == '/':
-            sum = 1.
+
         for value in values:
-            parts = value.split('\t')
+            parts = value.split('|')
             val2 = float(parts[-1])
-            if parts[0] == 'NODE' and key != '/':
-                if sum == None:
+            if objtype == 'NODE':
+                if sum is None:
                     sum = val2
                 else:
                     if conjunction:
                         sum = parallel(sum, val2)
                     else:
                         sum += val2
-                        
-            if parts[0] == 'edge':
-                type = parts[1]
-                end = parts[2]
-                weight = float(parts[3])
-                node_score = sum*weight
-                edge_value = u"edge\t%(type)s\t%(end)s\t%(weight)s" % dict(
-                  type=type, end=end, weight=weight
-                )
-                yield key, edge_value
-                node_value = u"NODE\t%s" % node_score
-                yield end, node_value
-
-    def reduce_nop(self, key, values):
-        for value in values:
-            yield key, value
+        if sum:
+            output = {'key': key, 'score': sum}
+            yield key, output
 
     def steps(self):
         return [self.mr(self.map_json, self.reduce_activation),
                 self.mr(None, self.reduce_activation),
                 self.mr(None, self.reduce_activation),
-                self.mr(None, self.reduce_activation)]
+                self.mr(None, self.reduce_activation),
+                #self.mr(None, self.reduce_nop),]
+                self.mr(None, self.reduce_final)]
 
 if __name__ == '__main__':
     CORONAForwardStep.run()
